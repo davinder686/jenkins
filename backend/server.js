0c7c3fb0f942cc913b132da654de0b78a2b1d523
Davinder Singh Ajji <davindersinghajji1996@gmail.com>
2022-01-14 14:54:10 +0530

    default get method added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ app.use(express.json())
 app.use(cors("*"))
 app.use("/user",userRouter)
 
+app.use("/",(request,response)=>
+{
+    response.send(`hello from server you are in....`)
+})
+
 app.listen(4000,"0.0.0.0",()=>
 {
     console.log(`server started on port 4000`)
